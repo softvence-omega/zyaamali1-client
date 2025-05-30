7a48155b49e597a59871edfdec43494fff93cb74
Md-Sifat-code <codewithsifat4@gmail.com>
2025-05-31 02:35:14 +0600

    Done the Subscription and billing fully with responsive

--- src/Dashboard/DashboadLayout/DashboardLayout.tsx
+++ src/Dashboard/DashboadLayout/DashboardLayout.tsx
@@ -4,7 +4,9 @@ import { Outlet } from "react-router-dom";
 const DashboardLayout = () => {
   return (
     <div className="flex px-4 py-8 gap-x-10 min-h-screen">
-      <Sidebar />
+      <div className="hidden md:block h-fit">
+        <Sidebar />
+      </div>
 
       {/* Right Content Area */}
       <div className="flex-1">
