1af481baa33879e9970b75b8b8d22ca7d61f7ea8
Md-Sifat-code <codewithsifat4@gmail.com>
2025-05-30 06:09:54 +0600

    done the chat picture

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,5 +1,27 @@
+import chat from "../assets/chatpic.png"
 const Services = () => {
-  return <div className="h-screen">Services</div>;
+  return <section>
+    <div>
+      <div>
+        {/* for the upper description */}
+      </div>
+      <div className="flex flex-col items-center justify-center py-12">
+        {/* for the chat pic */}
+        <img src={chat} alt="" />
+      </div>
+      <div>
+        {/* for another description */}
+      </div>
+      <div>
+        {/* for the grid system */}
+      </div>
+      <div>
+        {/* for the banner system */}
+      </div>
+
+    </div>
+  </section>;
 };
 
 export default Services;
+  
