2cb7586b83cdd47102293da2e233ba94aa16cbc0
Sohag00879 <shishirsohagislam@gmail.com>
2025-05-31 06:10:20 +0600

    resolve

--- src/Dashboard/DashboadLayout/DashboardLayout.tsx
+++ src/Dashboard/DashboadLayout/DashboardLayout.tsx
@@ -1,16 +1,14 @@
 import Sidebar from "@/components/Dashboard/Sidebar";
-import SidebarExpand from "@/components/Dashboard/SidebarExpand";
 import { Outlet } from "react-router-dom";
 
 const DashboardLayout = () => {
   return (
     <div className="flex px-4 py-8 gap-x-10 min-h-screen">
-      <div className="block lg:hidden h-fit">
+      <div className="hidden md:block h-fit">
         <Sidebar />
       </div>
-      <div className="hidden lg:block h-fit">
-        <SidebarExpand />
-      </div>
+
+      {/* Right Content Area */}
       <div className="flex-1">
         <Outlet />
       </div>
