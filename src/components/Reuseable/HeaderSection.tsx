d122fc616d3b472c10ec9e47a375f75d918d3205
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2025-06-02 03:17:21 +0600

    updated the analytics page

--- src/components/Reuseable/HeaderSection.tsx
+++ src/components/Reuseable/HeaderSection.tsx
@@ -9,21 +9,7 @@ type SectionTitleProps = {
 const HeaderSection: React.FC<SectionTitleProps> = ({ title, subtitle }) => {
   return (
     <div>
-    <div>
-          <h2 className="mb-2 text-2xl font-bold bg-clip-text text-transparent"
-        style={{
-          backgroundImage: 'linear-gradient(90deg, #000000 4.62%, #8E6EFF 102.8%)',
-          WebkitBackgroundClip: 'text',
-          WebkitTextFillColor: 'transparent',
-        }}
-      >
-        {title}
-      </h2>
-      {subtitle && (
-        <p className="text-T-600 text-base sm:text-lg">{subtitle}</p>
-      )}
-    </div>
-
+   
      
     </div>
   );
