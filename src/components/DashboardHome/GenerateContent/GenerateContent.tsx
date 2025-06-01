533fb859993999b2ce67c9127d086fec9386361a
Sazzad-Hosen-Mahim <sajjadhosenmahim@gmail.com>
2025-06-02 03:18:59 +0600

    chat ui completed

--- src/components/DashboardHome/GenerateContent/GenerateContent.tsx
+++ src/components/DashboardHome/GenerateContent/GenerateContent.tsx
@@ -1,7 +1,10 @@
+import ChatUI from "./ChatUI";
+
 const GenerateContent = () => {
   return (
     <div className="w-full h-screen bg-Foundation-gray-bg">
-      <div className="w-full h-[72px] bg-blue-900"></div>
+      <div className="w-full h-[72px] bg-blue-900" />
+      <ChatUI />
     </div>
   );
 };
