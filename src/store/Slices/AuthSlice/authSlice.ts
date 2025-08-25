6a7fc4182e04fcf172e50178b844fb2c67d118a5
Md Soyaib Hossain <mdsoyaibhossain@macmini24.local>
2025-08-25 09:02:57 +0600

    Auto commit: Modified package-lock.json, Modified package.json, Modified src/Auth/Auth_Components/Login.tsx, Modified src/store/Slices/AuthSlice/authSlice.ts

--- src/store/Slices/AuthSlice/authSlice.ts
+++ src/store/Slices/AuthSlice/authSlice.ts
@@ -1,22 +1,32 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-interface AuthState {
-  user: { role: string } | null;
-}
+export type TUser = {
+  role?: string;
+  email: string;
+};
 
-const initialState: AuthState = {
-  user: { role: "user" },
+type TAuthState = {
+  user: null | TUser;
+  token: null | string;
+};
+
+const initialState: TAuthState = {
+  user: null,
+  token: null,
 };
 
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
+    login: (state, action: PayloadAction<TUser>) => {
+      state.user = action.payload;
+    },
     logout: (state) => {
       state.user = null;
     },
   },
 });
 
-export const { logout } = authSlice.actions;
+export const { login, logout } = authSlice.actions;
 export default authSlice.reducer;
