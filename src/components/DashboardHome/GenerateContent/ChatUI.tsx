7a987883c7623ecb5676dca7f53ec5b8ed7c3b2d
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2025-09-03 09:38:21 +0600

    Auto commit: Modified src/components/DashboardHome/GenerateContent/ChatUI.tsx, Deleted src/store/Features/chatBot/chatBotApi.ts

--- src/components/DashboardHome/GenerateContent/ChatUI.tsx
+++ src/components/DashboardHome/GenerateContent/ChatUI.tsx
@@ -1,5 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { usePostChatMutation } from "@/store/Features/chatBot/chatBotApi";
 import { RootState } from "@/store/store";
 import axios from "axios";
 import { useState, useEffect } from "react";
