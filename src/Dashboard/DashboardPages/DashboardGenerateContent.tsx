70f196fbcde690a6dd82af19ec84ea4779220719
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2025-09-03 10:28:21 +0600

    Auto commit: Modified src/Dashboard/DashboardPages/DashboardGenerateContent.tsx

--- src/Dashboard/DashboardPages/DashboardGenerateContent.tsx
+++ src/Dashboard/DashboardPages/DashboardGenerateContent.tsx
@@ -1,4 +1,5 @@
 import Sidebar from "@/components/Dashboard/Sidebar";
+import SidebarExpand from "@/components/Dashboard/SidebarExpand";
 import GenerateContent from "@/components/DashboardHome/GenerateContent/GenerateContent";
 import GenerateSidebar from "@/components/DashboardHome/GenerateContent/GenerateSidebar";
 
@@ -9,6 +10,14 @@ const DashboardGenerateContent = () => {
       <div className="hidden lg:block h-[200px] mt-8 mx-5">
         <Sidebar />
       </div>
+
+        {/* <div className="col-span-2 block xl:hidden h-fit sticky top-8">
+        <Sidebar />
+      </div>
+      <div className="hidden col-span-2 xl:block h-fit sticky top-8">
+        <SidebarExpand />
+      </div> */}
+
       {/* sidebar  */}
       <GenerateSidebar />
       <GenerateContent />
