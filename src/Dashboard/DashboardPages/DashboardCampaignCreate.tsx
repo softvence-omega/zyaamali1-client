9fe4ef139c85c0343be9af969c1c5e7a0c6f072a
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2025-09-04 14:47:09 +0600

    Auto commit: Modified src/Dashboard/DashboardPages/DashboardCampaignCreate.tsx

--- src/Dashboard/DashboardPages/DashboardCampaignCreate.tsx
+++ src/Dashboard/DashboardPages/DashboardCampaignCreate.tsx
@@ -209,7 +209,7 @@ const DashboardCampaignCreate = () => {
   const [carousel, setCarousel] = useState<FileList | null>(null);
   const [loading, setLoading] = useState(false);
 
-  console.log("always ", locationCode, locationId);
+  // console.log("always ", locationCode, locationId);
 
   const validateForm = () => {
     const newErrors: Record<string, string> = {};
@@ -482,7 +482,7 @@ const DashboardCampaignCreate = () => {
     }
   };
 
-  console.log("state of loading", loading);
+  // console.log("state of loading", loading);
 
   return (
     <div>
@@ -1091,32 +1091,32 @@ const DashboardCampaignCreate = () => {
             <OutlineButton>Save Draft</OutlineButton>
             <PrimaryButton
               onClick={handleSubmit}
-             
-              className="flex items-center gap-2"
             >
-              {loading && (
-                <svg
-                  className="animate-spin h-5 w-5 text-white"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <circle
-                    className="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    strokeWidth="4"
-                  />
-                  <path
-                    className="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
-                  />
-                </svg>
-              )}
-              {loading ? "Launching Campaign..." : "Launch Campaign"}
+              <span className="flex items-center gap-2">
+                {loading && (
+                  <svg
+                    className="animate-spin h-5 w-5 text-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    />
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+                    />
+                  </svg>
+                )}
+                {loading ? "Launching Campaign..." : "Launch Campaign"}
+              </span>
             </PrimaryButton>
           </div>
 
