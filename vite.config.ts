a96c6626355a4f24c32776b2bb56c004a47f762b
arafat2020 <arafatmannan2019@gmail.com>
2025-10-25 09:04:17 +0600

    MOD: Minor adjustment

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,30 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    host: true,
+    port: 3001,
+    strictPort: true,
+    origin: "http://0.0.0.0:3000",
+    allowedHosts: [
+     "adelo.ai",
+      "localhost",
+    ],
+  },
+  preview: {
+    port: 3001,
+    strictPort: true,
+    allowedHosts: [
+       "adelo.ai",
+      "localhost",
+    ],
+  },
 });
+
